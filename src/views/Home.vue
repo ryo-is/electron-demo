b0dcc39af656385296396eadf23606457e9eb559
is_ryo <izumiryousuke@izumiryousukenoMacBook-Pro.local>
2020-05-04 18:31:55 +0900

    change pug & typescript

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home
+    .title home
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from 'vue'
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default Vue.extend({
+  name: 'Home',
+})
 </script>
