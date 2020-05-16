c8a9101f6a443303558434ff6b5be4db6930d9c0
is_ryo <izumiryousuke@izumiryousukenoMacBook-Pro.local>
2020-05-16 21:12:22 +0900

    add render notification

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,10 +3,20 @@
     .title.py-4 Main View
     v-divider
     v-btn(to="/users") users
+    v-btn(@click="onNotification") notification
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  methods: {
+    onNotification() {
+      // actionはServiceWorker内でのみサポート
+      new Notification('notification', {
+        body: 'notification body',
+      })
+    },
+  },
+})
 </script>
